1fb3fd4f687469eb229e20b411f2e85b4eff7789
Vaidas Budrys <vaidas.budrys3@gmail.com>
2022-11-17 21:07:30 -0500

    disable desktop code

--- src/display_desktop.cpp
+++ src/display_desktop.cpp
@@ -3,23 +3,23 @@
 ////
 #ifndef RGB_MATRIX_PLATFORM_RPI
 
-#include "../include/display.h"
-
-void PixelDisplay::DisplayImage(const Mat &material)
-{
-    Mat enlarged;
-    resize(material, enlarged, Size(_displayDimension, _displayDimension), 0, 0, INTER_AREA);
-    imshow(_windowName, enlarged);
-    waitKey(25);
-}
-
-PixelDisplay::~PixelDisplay()
-{
-    destroyWindow(_windowName);
-}
-
-PixelDisplay::PixelDisplay(int argc, char** argv)
-{
-    namedWindow(_windowName);
-}
+//#include "../include/display.h"
+//
+//void PixelDisplay::DisplayImage(const Mat &material)
+//{
+//    Mat enlarged;
+//    resize(material, enlarged, Size(_displayDimension, _displayDimension), 0, 0, INTER_AREA);
+//    imshow(_windowName, enlarged);
+//    waitKey(25);
+//}
+//
+//PixelDisplay::~PixelDisplay()
+//{
+//    destroyWindow(_windowName);
+//}
+//
+//PixelDisplay::PixelDisplay(int argc, char** argv)
+//{
+//    namedWindow(_windowName);
+//}
 #endif
